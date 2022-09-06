29926ca8f6c4da20a7a8088b16b62c30de7eb867
Kapikologamer <62207823+Kapikologamer@users.noreply.github.com>
2022-09-07 00:59:57 +0200

    0.1 beta
    RELEASE no. 1:
    GUI update

---
+++ tells.py
@@ -0,0 +1,28 @@
+import json
+import colorama
+from colorama import Back, Fore, Style
+
+colorama.init()
+
+print()
+print()
+print(Fore.RED + "          //////// ////// //    //    /////")
+print(Fore.YELLOW + "             //    ////// //    //    ///  ")
+print(Fore.GREEN + "             //    //     //    //    ///  ")
+print(Fore.CYAN + "             //    //     //    //    ///  ")
+print(Fore.LIGHTBLUE_EX + "             //    ////// //    //      ///")
+print(Fore.BLUE + "             //    ////// //    //      ///")
+print(Fore.LIGHTMAGENTA_EX + "             //    //     //    //      ///")
+print(Fore.MAGENTA + "             //    ////// //    //      ///")
+print(Fore.LIGHTBLACK_EX + "             //    ////// ///// ///// /////")
+print()
+print()
+print(Fore.LIGHTBLUE_EX + "   *           WITAJ W PROGRAMIE TELLS          *")
+print()
+print(Fore.LIGHTCYAN_EX + "   *     1. ZALOGUJ SIĘ        " + Fore.LIGHTGREEN_EX + "*     2. ZAREJESTRUJ SIĘ")
+print()
+print(Fore.LIGHTCYAN_EX + "   *     3. PISZ ANONIMOWO     " + Fore.LIGHTGREEN_EX + "*     4. SPRAWDŹ KOD ŹRÓDŁOWY")
+print()
+print()
+input(">>> ")
+
